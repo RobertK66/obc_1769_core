bf0aa6c75130f12e3f3be9681a96472fdd1e74b2
Potulov Jevgeni <118033@fhwn.ac.at>
2022-08-06 14:33:48 +0200

    Code clean up 2

--- src/mod/thr/thr.c
+++ src/mod/thr/thr.c
@@ -16,7 +16,6 @@ void thrUartIRQ(LPC_USART_T *pUART);
 void thrProcessRxByte(uint8_t rxByte);
 void thr_debugPrintBuffer(uint8_t *buffer,int bufferlen);
 
-// local/module variables
 static thr_initdata_t *thrInitData;
 
 // ************************** TX Circular byte Buffer Begin ********************
@@ -28,12 +27,10 @@ static bool		  	   thrTxBufferFull = false;
 static bool				thrFirstByteAfterReset=true;
 
 
-/////////////////// MODULE LOCAL VARIABLES////////////
 
 #define THR_BUFFERLEN 700 // maximum buffer length
 //int thr_counter = 0; would be defined in l4_thruster
 char thr_receiveBuffer[THR_BUFFERLEN] ="";
-
 //l4_thr_counter = 0; // set received bytes counter to 0 initialy // defined in l4_thruster.c
 
 
@@ -77,23 +74,14 @@ void thrInit (void *initData) {
 
 	// Switch the 'enable' pin to low (no internal Volage regulator needed
 	//Chip_GPIO_SetPinOutLow(LPC_GPIO, thrInitData->pEnablePin->pingrp, thrInitData->pEnablePin->pinnum);
-
 	//// IMPORTANT PINIDX_RS485_TX_RX   HIGH - TRANSMIT              LOW - RECEIVE
-
 	// SET TO RECEIVE UPON INITIALIZATION
 	//Chip_GPIO_SetPinOutLow(LPC_GPIO, 2, 5); //  This is PINIDX_RS485_TX_RX RECEIVE
 	//Chip_GPIO_SetPinOutHigh(LPC_GPIO, 2, 5); //  This is PINIDX_RS485_TX_RX TRANSMIT
 
-
-
 	// Init UART
 	InitUart(thrInitData->pUart, 115200, thrUartIRQ);
 	thrFirstByteAfterReset = true;
-
-	//Enable Auto direction controll fo UART1
-	//uint32_t uart_bit_mask = 1 << 4; // DCRTL at 4th bit position
-	//LPC_UART1->RS485CTRL = ( ( LPC_UART1->RS485CTRL &~ uart_bit_mask)  |   (1<<4)        );
-	// NOTE - THIS HAD NO EFFECT ON RS485 Dirrection controll perfomance
 
 	// Enable dirrection controll
 	LPC_UART1->RS485CTRL |= UART_RS485CTRL_DCTRL_EN; // Enable Auto Direction Control
@@ -174,46 +162,21 @@ void thrSendByte(uint8_t b) {
 void thrSendBytes(uint8_t *data, uint8_t len) {
 	//set to transmit when sending data package
 	Chip_GPIO_SetPinOutHigh(LPC_GPIO, 2, 5); //  This is PINIDX_RS485_TX_RX TRANSMIT
-	/*
-	for (int i=0;i<1000000;i++) {
-			// DELAY
-		}
-	*/
 	for (int i=0;i<len;i++) {
 		thrSendByte(data[i]);
 	}
-	/*
-	for (int i=0;i<1000000;i++) {
-				// DELAY
-			}
-
-	*/
-	// switch back to receive when data transmission end
-	//Chip_GPIO_SetPinOutLow(LPC_GPIO, 2, 5); //  This is PINIDX_RS485_TX_RX RECEIVE
-
-	// SWITCHING BACK TO RX IS MOVED INTO void thrUartIRQ(LPC_USART_T *pUART)
-	// No delays
 
 }
 
 
 void thrProcessRxByte(uint8_t rxByte) {
 	// do your processing of RX here....
-	// Simply print RX into debug UART
-	//char* Byte;
 
-	//Byte = (char*) rxByte;
-	//SysEvent(MODULE_ID_CLIMBAPP, EVENT_INFO, EID_APP_STRING, Byte, strlen(Byte));
-
-	//if (thr_counter<= THR_BUFFERLEN){
 	if (l4_thr_counter<= l4_thr_ExpectedReceiveBuffer){ // change it to expected buffer length SET by REQUEST functions
 
 		thr_receiveBuffer[l4_thr_counter]=(char) rxByte;
 		Chip_UART_SendByte(LPC_UART2, rxByte); // print received byte
-
-
 		l4_thr_counter++;
-
 	}
 	else {
 		l4_thr_counter =0;
@@ -224,8 +187,6 @@ void thrProcessRxByte(uint8_t rxByte) {
 
 	}
 
-
-
 }
 
 
@@ -235,7 +196,6 @@ void thrProcessRxByte(uint8_t rxByte) {
 void thr_debugPrintBuffer(uint8_t *buffer,int bufferlen){
 
 	//LPC_UART2 is debug UART
-
 	for (int i=0;i<bufferlen;i++){
 		Chip_UART_SendByte(LPC_UART2, buffer[i]);
 
