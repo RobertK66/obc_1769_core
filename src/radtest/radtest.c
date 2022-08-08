3f941422e3959f7d2f30eb6748cac7de81850cf3
RobertK66 <RobertK66@users.noreply.github.com>
2022-08-08 18:46:37 +0200

    make a (temp) radtest module for autostart and control of all tests

---
+++ src/radtest/radtest.c
@@ -0,0 +1,10 @@
+/*
+ * radtest.c
+ *
+ *  Created on: 08.08.2022
+ *      Author: Robert
+ */
+
+// This is a temporary module to implement radiation test code for 2022-08-20 Tests.
+// Will be removed again later.
+
---
+++ src/radtest/radtest.h
@@ -0,0 +1,13 @@
+/*
+ * radtest.h
+ *
+ *  Created on: 08.08.2022
+ *      Author: Robert
+ */
+
+#ifndef RADTEST_RADTEST_H_
+#define RADTEST_RADTEST_H_
+
+
+
+#endif /* RADTEST_RADTEST_H_ */
