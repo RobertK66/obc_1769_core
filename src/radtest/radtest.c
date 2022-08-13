ffdf7c7c32a7ad235bc75e081a7211b7a019c564
RobertK66 <RobertK66@users.noreply.github.com>
2022-08-13 13:11:07 +0200

    add some options for using string literals. (C-Tutorial ;-) )

--- src/radtest/radtest.c
+++ src/radtest/radtest.c
@@ -46,6 +46,8 @@ void rtst_init (void *initData) {
 	InitTimer(RtstTimerPtr, RTST_TICK_MS, rtst_timer_IRQHandler);
 }
 
+#define CTUT_3	// CTUT_1 CTUT_2  CTUT_3
+
 void rtst_main (void){
 	if (RtstTick) {
 		// Called once every second.
@@ -54,17 +56,31 @@ void rtst_main (void){
 
 		if ((RtstTickCnt % RTST_HEARTBEAT_TICKS) == 0) {
 			// Make heartbeat every 10 sec.
-			uint8_t request[26];
-			request[0]= 0x53;
+#ifdef CTUT_1
+			// A short C-Tutorial Option 1 - your code with my comments
+			uint8_t request[26];		// Compiler reserves space for 26 uint8_t variables on current stack (-> 26 bytes used on stack - not initilaized filled with random stuff)
+										// Compiler gives you access to an (uint8_t *) - read "an uint8_t Pointer" - which is initialized to point to the first uint8t byte on the stack
+			                            //          under the name 'request'
+										// I think the "pointer request" also goes to the stack here (having 4 more bytes used for it) but I am not sure about this
+
+			request[0]= 0x53;			// left side: compiler takes the content of "uint8_t Pointer named request" ...
+										//			  adds 0 times the size of a uint8_t variable (1) -> which here does nothing to the pointer
+										//            and stores the const from the right side 0x53 into the porition where the calculated pointer points to.
+										// complicated way to say that the [x] operator after a pointer variable treats the memory beeing available as "Array of element types"
+			                            //            where here elemnt type is uint8_t (needing 1 byte of memory space)
 			request[1]= 0x75;
 			request[2]= 0x70;
 			request[3]= 0x65;
-			request[4]= 0x72;
-			request[5]= 0x76;
-			request[6]= 0x69;
-			request[7]= 0x73;
-			request[8]= 0x69;
-			request[9]= 0x6f;
+
+			// To make this 'meters of code' ;-) more readable you could write it like this:
+			request[4]= 'r';	// -> depending of compiler warning levels this could lead to a warning here because 'x' is treated as type "char" and it has to be casted to uint8_t to fit your variable type.
+								// My IDE here gives no warnings ->?
+			// or if there is a warning and you want to avoid this:
+			request[5]= (uint8_t)'v'; // with the 'cast' operator (uint8_t) you can tell the compiler that you know that this is what you intend here and it should not complain about it!
+			request[6]= 'i'; //0x69;
+			request[7]= 's'; //0x73;
+			request[8]= 'i'; //0x69;
+			request[9]= 'o'; // 0x6f;   ok I think you get it ;-) would be much more readable and checkable with char ' ' constants!!!!
 			request[10]= 0x6e;
 			request[11]= 0x20;
 			request[12]= 0x77;
@@ -81,9 +97,43 @@ void rtst_main (void){
 			request[23]= 0x65;
 			request[24]= 0x64;
 			request[25]= 0x0a;
-
-			uint8_t len = sizeof(request);
+			uint8_t len = sizeof(request);		// This sizeof() is calculated by the compiler to be exactly the 26 you wrote in line 61. Nothing done at runtime here.
 			print_pure_debug(request, len);
+#endif
+			
+#ifdef CTUT_2
+			const char *request = "Supervision watchdog feed\n";	// Here the compiler puts the string literal from the right side
+																	// somewhere into read only memory including a 0x00 or '\0' char as
+			                                                		// as end of string marker.
+															// The (char *) variable 'request' is put on the stack memory and filled with the address of the literal
+															// -> pointing to the first char.
+			// So here we have similar status as in line 100 in CTUT_1 above!
+			// Difference is we have (char *) and not a (uint8_t *) and the string is 'read only'
+
+			// now how to we get length !?
+			// it would be possible with:
+			uint8_t len = strlen(request);		// This 'calculates the char from pointer (base) up to the \0
+												// I am pretty sure this is evaluated at compile time and not a call at runtime (but this is not 100% sure....)
+
+			// In order to avoid the compiler warning here we have to explicitly cast the request variable beeing a (char *) to (uint8_t *)
+			// This does nothing in code or at runtime. It only tells the compiler that he should shut up with his warning. We know what we do here ;-)!
+			print_pure_debug((uint8_t *)request, len);
+#endif
+
+
+#ifdef CTUT_3
+			// If you want to avoid the strlen you could also use sizeof like this:
+
+#define MY_OUTPUT_STRING "Supervision watchdog feed\n"		// We need the string literal 2 times now so use #define  to make it unique and usable as often we want...
+
+			const char *request =  MY_OUTPUT_STRING;
+			uint8_t len = sizeof(MY_OUTPUT_STRING);
+			print_pure_debug((uint8_t *)request, len);
+
+
+#endif
+			
+			
 		}
 		if ((RtstTickCnt % RTST_MEMTST_TICKS) == 0) {
 			// Memory tests TODO
