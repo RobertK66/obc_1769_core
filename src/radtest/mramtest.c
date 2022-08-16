c0a7caab17f4704ad27a24411db938708762d299
RobertK66 <RobertK66@users.noreply.github.com>
2022-08-16 09:42:50 +0200

    spülit the flash area in 2 sections and put some testpetterns at start of flash2 !!!

--- src/radtest/mramtest.c
+++ src/radtest/mramtest.c
@@ -10,8 +10,18 @@
 
 #include "mramtest.h"
 
+uint8_t 		*expectedPagePatternsPtr; 						// points to fillpattern bytes[0..7] (in flash)
+extern uint8_t 	*expectedPagePatternsPtr; 						// points fillpattern bytes[0..3]
+
+
+void rtst_memtestinit(void) {
+	expectedPagePatternsPtr = &expectedPagePatternsSeq[0];		// Reset the expected Pattern pointer to start value (possible: 0 ...3)
+
+}
+
+
 void rtst_mramtick(void) {
-	// ....
+
 }
 
 #endif /* RADTEST_MRAMTESTS_C_ */
--- src/radtest/mramtest.h
+++ src/radtest/mramtest.h
@@ -8,6 +8,15 @@
 #ifndef RADTEST_MRAMTEST_H_
 #define RADTEST_MRAMTEST_H_
 
+#include <chip.h>
+
+#define RADTST_PRGFLASH2_TARGET_PAGES 		8
+#define RADTST_PRGFLASH2_TARGET_PAGESIZE 1024
+
+#define RADTST_EXPECTED_PATTERN_CNT				4
+extern uint8_t expectedPagePatternsSeq[2 * RADTST_EXPECTED_PATTERN_CNT];
+extern uint8_t prgFlash2Target[RADTST_PRGFLASH2_TARGET_PAGES][RADTST_PRGFLASH2_TARGET_PAGESIZE];
+
 void rtst_mramtick(void);
 
 #endif /* RADTEST_MRAMTEST_H_ */
